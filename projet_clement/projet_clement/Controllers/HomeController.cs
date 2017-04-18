e35ff49a76cca323ba78d2aeb2819390f6304e25
Unknown <Clement.henriet@gmail.com>
2017-04-18 17:00:08 +0200

    ajout listes et commentaires

---
+++ projet_clement/projet_clement/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using projet_clement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projet_clement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MvcEntities db = new MvcEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var SansTel = db.Users.Where(x => x.Numero == null).FirstOrDefault(); 
+            /* recupere dans la base de donnée Users la ligne id de celui qui n'a pas 
+             * de numero de telephone. la recherche s'arrete à la premiere ligne 
+             * trouvée sans numero de telephone            
+             * */
+            var allUsers = db.Users.Where(x => x.Nom != null);
+            /* recupere dans la base de données User les id de toutes les lignes ou la
+             * colonne nom n'est pas nulle (ou elle est remplie)
+             * */
+
+            var ArtSanstel = db.articles.Where(x => x.userId == SansTel.userId).ToList();
+            /* recupere dans la Bd articles tous les articles qui ont le meme UserId 
+             * que celui qui n'a pas de telephone dans la base de donnée Users 
+             * et on en fait une liste
+             * */
+
+
+            var model = new HomeModel(allUsers, ArtSanstel);
+
+            return View(model);
+        }
+    }
+}
---
+++ projet_clement/projet_clement/Models/HomeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projet_clement.Models
+{
+    public class HomeModel
+    {
+        public List<Users> UserList { get; set; } //creation classe Userlist typée Users
+        public List<articles> ArticlesList { get; set;  }//creation classe ArticlesList typée articles
+
+        public List<int> Naissances { get; set; } //creation classe Naissances typée int
+
+       public HomeModel(IQueryable<Users> allUsers, List<articles> articles)
+       {
+            Naissances = new List<int>(allUsers.Select(x => x.Naissance.Value.Year));
+            UserList = new List<Users>(allUsers);
+            ArticlesList = new List<articles>(articles);
+
+       }
+    }
+}
