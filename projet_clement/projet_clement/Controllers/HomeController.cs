cf5926f766db898ec6cb21acbfece507b1348e51
Unknown <Clement.henriet@gmail.com>
2017-04-19 12:07:48 +0200

    ajout page édition users

--- projet_clement/projet_clement/Controllers/HomeController.cs
+++ projet_clement/projet_clement/Controllers/HomeController.cs
@@ -23,16 +23,61 @@ namespace projet_clement.Controllers
              * colonne nom n'est pas nulle (ou elle est remplie)
              * */
 
-            var ArtSanstel = db.articles.Where(x => x.userId == SansTel.userId).ToList();
+            var ArtSanstel = db.articles.Where(x => x.userId == SansTel.userId);
             /* recupere dans la Bd articles tous les articles qui ont le meme UserId 
              * que celui qui n'a pas de telephone dans la base de donnée Users 
              * et on en fait une liste
              * */
 
+            var TitreArt = db.articles.Where(x => x.contenu != null);
 
-            var model = new HomeModel(allUsers, ArtSanstel);
+
+            var model = new HomeModel(allUsers, ArtSanstel, TitreArt);
 
             return View(model);
+
+        }
+
+
+
+
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+          
+            var allUsers = db.Users.Where(x => x.Nom != null);
+            /* recupere dans la base de données User les id de toutes les lignes ou la
+             * colonne nom n'est pas nulle (ou elle est remplie)
+             * */
+            var model = new HomeModel(allUsers);
+
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Users formUser)
+        {
+            //user dans la bdd
+            var dbUser = db.Users.FirstOrDefault(x => x.userId == id);
+
+            dbUser.Nom = formUser.Nom;
+            dbUser.Prenom = formUser.Prenom;
+            dbUser.Naissance = formUser.Naissance;
+            dbUser.Numero = formUser.Numero;
+            dbUser.Departement = formUser.Departement;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                return View(e);
+            }
+
+            return RedirectToAction("edit", "Home");
         }
     }
 }
--- projet_clement/projet_clement/Models/HomeModel.cs
+++ projet_clement/projet_clement/Models/HomeModel.cs
@@ -11,13 +11,23 @@ namespace projet_clement.Models
         public List<articles> ArticlesList { get; set;  }//creation classe ArticlesList typée articles
 
         public List<int> Naissances { get; set; } //creation classe Naissances typée int
+        /* ici on crée quelque chose de vide, c'est juste un plan qu'on doit remplir ou utiliser plus
+         * tard
+         * */
+        public List<articles> TitreArticle { get; set;}
+        public List<articles> ArtSsTel { get; set; }
 
-       public HomeModel(IQueryable<Users> allUsers, List<articles> articles)
+
+       public HomeModel(IQueryable<Users> allUsers, IQueryable<articles> ArtSansTel, IQueryable<articles> TitreArt)
        {
-            Naissances = new List<int>(allUsers.Select(x => x.Naissance.Value.Year));
             UserList = new List<Users>(allUsers);
-            ArticlesList = new List<articles>(articles);
-
+            ArtSsTel = new List<articles>(ArtSansTel);
+            TitreArticle = new List<articles>(TitreArt);
        }
+
+        public HomeModel(IQueryable<Users> allUsers)
+        {
+            UserList = new List<Users>(allUsers);
+        }
     }
 }
